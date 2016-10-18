a1f535afc86b6b6c9dc5cf2856810a791728584b
sushiljainam <sushiljain1993@gmail.com>
2016-10-18 13:38:25 +0530

    parsing path 1/n

--- router.js
+++ router.js
@@ -1,5 +1,8 @@
+var init = require('./init');
+
 function route(handle,htmlPath,pathname,response,reviewData){
 	console.log("Routing a reqest for " + pathname);
+  getUserParsePath(pathname);
 	if(typeof handle[pathname] === 'function'){
 		handle[pathname](htmlPath,response,reviewData);
 	}
@@ -9,5 +12,9 @@ function route(handle,htmlPath,pathname,response,reviewData){
 		response.write("Error 404 page not found");
 		response.end();
 	}
+}
+function getUserParsePath(pathname) {
+  ;
+  console.log(pathname.split("/user/"));
 }
 exports.route = route;
